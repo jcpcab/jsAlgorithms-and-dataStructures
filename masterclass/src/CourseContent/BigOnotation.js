23ca27142a20a66880a52dabf6452fc189992a8d
jcpcabanada <jcpcabanada@gmail.com>
2022-07-05 23:25:02 -0700

    initial

---
+++ masterclass/src/CourseContent/BigOnotation.js
@@ -0,0 +1,44 @@
+//BIG O NOTATIONS : A way to formalize fuzzy counting /  Allows us to talk formally about how the runtime of an algorithm grows as the inputs grow
+
+
+
+//      •It's important to have a precise vocabulary to talk about how our code performs
+//      •Useful for discussing trade-offs between different approaches
+//      •When your code slows down or crashes,identifying parts of the code that are inefficient can help us find pain points in our applications
+//       •Less important:it comes up in interviews!
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
